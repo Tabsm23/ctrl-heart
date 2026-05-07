a166877a6a986195c402d3900844b614d92543f9
Abigaile Mercurio <sumpark@Abigailes-MacBook-Air.local>
2026-05-07 22:47:00 +0700

    fix: use env vars for Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yvxooxyktmuifahrnamw.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2eG9veHlrdG11aWZhaHJuYW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMjQxMzEsImV4cCI6MjA5MjkwMDEzMX0.dLzGFZmo0w6TlJjEvqZzTFgCPZJ6Jjrtkzjmf-h8O4Y'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
